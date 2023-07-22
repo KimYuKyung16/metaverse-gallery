9c4a975c0aa4a39bda3a0b1f2447d20db798cee8
kyk <kyg0954@naver.com>
2023-07-22 16:16:09 +0900

    Refactor: 네비게이션바 관련 폴더 정리

--- navbar/navbar.php
+++ navbar/navbar.php
@@ -1,13 +1,12 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
   <meta charset="UTF-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <!-- <link rel="stylesheet" href="./navbar.css"> -->
-  <link href="https://fonts.googleapis.com/css2?family=Handlee&family=Nanum+Gothic+Coding&display=swap" rel="stylesheet">  
-  <title>Document</title>
-
+  <link rel="stylesheet" href="../css/navbar.css">
+  <link href="https://fonts.googleapis.com/css2?family=Handlee&family=Nanum+Gothic+Coding&display=swap" rel="stylesheet">
 </head>
 
 <?php
@@ -15,21 +14,18 @@ session_start();
 header('Content-Type: charset=utf-8');
 
 if (isset($_SESSION['id'])) { //로그인O
-  $login_bool = 1; 
+  $login_bool = 1;
 } else { //로그인X
   $login_bool = 0;
 }
 
-$conn = mysqli_connect('localhost', 'kyk', '3909', 'gallery_db');
-$sql = "
-          SELECT * FROM users WHERE 
-          u_id = '{$_SESSION['id']}' ";
+$conn = mysqli_connect('localhost', 'root', 'apmsetup', 'gallery_db');
+$sql = "SELECT * FROM users WHERE u_id = '{$_SESSION['id']}' ";
 $result = mysqli_query($conn, $sql);
 
 while ($row = mysqli_fetch_array($result)) {
   $gallery_num = $row['num'];
 }
-
 ?>
 
 <body>
@@ -38,9 +34,9 @@ while ($row = mysqli_fetch_array($result)) {
       <li id="title">Gallery</li>
 
       <ul id="navbar_menu_list">
-        <li><a href="#">갤러리 소개</a></li>
-        <li><a href="../gallery/oriental_gallery_list.php">진행중인 전시회</a></li>
-        <li><a href="../gallery/read.php?num=<?=$gallery_num?>">내 갤러리</a></li>
+        <li><a href="/gallery/galleryIntroduce/gallery_introduce.php">갤러리 소개</a></li>
+        <li><a href="/gallery/galleryInProgress/oriental_gallery_list.php">진행중인 전시회</a></li>
+        <li><a href="/gallery/myGallery/myGallery.php?num=<?= $gallery_num ?>">내 갤러리</a></li>
       </ul>
 
       <ul id="my_info_list_logout" style="display:flex">
@@ -56,19 +52,17 @@ while ($row = mysqli_fetch_array($result)) {
   </nav>
 </body>
 <script>
-const my_info_list_logout = document.getElementById("my_info_list_logout");
-const my_info_list_login = document.getElementById("my_info_list_login");
+  const my_info_list_logout = document.getElementById("my_info_list_logout");
+  const my_info_list_login = document.getElementById("my_info_list_login");
+  const login_bool = <?php echo $login_bool; ?>;
 
-var login_bool = <?php echo $login_bool;?>; 
-
-if (login_bool == 1) { //로그인o
-  my_info_list_logout.style.display = "none";
-  my_info_list_login.style.display = "flex";
-} else { //로그인x
-  my_info_list_logout.style.display = "flex";
-  my_info_list_login.style.display = "none";
-}
-
+  if (login_bool == 1) { //로그인o
+    my_info_list_logout.style.display = "none";
+    my_info_list_login.style.display = "flex";
+  } else { //로그인x
+    my_info_list_logout.style.display = "flex";
+    my_info_list_login.style.display = "none";
+  }
 </script>
-<script type="module" src="./navbar/navbar.js"></script>
+<script type="module" src="../js/navbar.js"></script>
 </html>
