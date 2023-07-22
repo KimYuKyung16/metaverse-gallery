e621673ba139c56cf4c7dcf1bf915de0952a14d5
kyk <kyg0954@naver.com>
2023-07-22 15:26:21 +0900

    Refactor: Auth 관련 파일 구조 정리

---
+++ auth/login.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html lang="ko">
+
+<head>
+  <meta charset="utf-8">
+  <link rel="stylesheet" href="../navbar/navbar.css">
+  <link rel="stylesheet" href="login.css">
+  <title>로그인</title>
+</head>
+
+<body>
+  <?php include "../navbar/navbar.php" ?> <!-- 네비게이션 바 부분 -->
+
+  <form id="login-Form" action="checkLogin.php" method="POST">
+    <div class="login_list-Div">
+      <h2>로그인</h2>
+      <hr noshade color="#BDBDBD">
+      <div class="login-2">
+        <label for="id" class="form_label">아이디</label>
+        <input id="id" class="form_control" name="id" type="text" placeholder="아이디">
+      </div>
+      <div class="login-2">
+        <label for="pw" class="form_label">비밀번호</label>
+        <input id="pw" class="form_control" name="pw" type="password" placeholder="비밀번호">
+      </div>
+      <button id="login_confirm_btn" type="submit" class="btn">로그인 하기</button>
+
+      <div class="bottomText">
+        <p>계정이 없다면 <a href="signup.php">Sign up</a>
+        <p>
+      </div>
+
+    </div>
+  </form>
+</body>
+<script type="module" src="login.js"></script>
+
+</html>
--- auth/signup.php
+++ auth/signup.php
@@ -2,20 +2,17 @@
 <html lang="en">
 
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-    <link rel="stylesheet" href="../navbar/navbar2.css">
-    <link rel="stylesheet" href="./signup1.css">
-    <!-- <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/5.0.0-alpha2/css/bootstrap.min.css" integrity="sha384-DhY6onE6f3zzKbjUPRc2hOzGAdEf4/Dz+WJwBvEYL/lkkIsI3ihufq9hk9K4lVoK" crossorigin="anonymous"> -->
-    <!-- <script src="https://stackpath.bootstrapcdn.com/bootstrap/5.0.0-alpha2/js/bootstrap.bundle.min.js" integrity="sha384-BOsAfwzjNJHrJ8cZidOg56tcQWfp6y72vEJ8xQ9w6Quywb24iOsW913URv1IS4GD" crossorigin="anonymous"></script> -->
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>회원가입</title>
+  <link rel="stylesheet" href="../navbar/navbar.css">
+  <link rel="stylesheet" href="signup.css">
 </head>
 
 <body>
+  <?php include "../navbar/navbar.php" ?>
 
-  <?php include "../navbar/navbar.php"?>  <!-- 네비게이션 바 부분 -->
 
-  
   <form action="signupProcess.php" method="POST" id="signup_form">
     <div class="signup_list_div">
       <h2>회원가입</h2>
@@ -33,17 +30,15 @@
         <input id="pw_check" class="form_control" name="pw_check" type="password" placeholder="비밀번호 확인">
       </div>
       <button id="signup_confirm_btn" class="signup_confirm_btn" type="button">회원가입</button>
-      
-      <div class="bottomText">
-        <p>계정이 있다면 <a href="#">로그인</a> 해주세요</p>
-      </div>  
-    
-    </div>
 
+      <div class="bottomText">
+        <p>계정이 있다면 <a href="login.php">로그인</a> 해주세요</p>
+      </div>
+    </div>
   </form>
 
-
 </body>
-<script type="module" src="./signup.js"></script>
-<script type="module" src="../navbar/navbar.js"></script>
+<script type="module" src="signup.js"></script>
+<script type="module" src="login.js"></script>
+
 </html>
